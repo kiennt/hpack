091dffc2ca458a7ef29f0bd382460f619ab01b55
Kien Nguyen Trung <trungkien2288@gmail.com>
2016-03-13 13:29:08 +0700

    Publish to hex

--- mix.exs
+++ mix.exs
@@ -5,28 +5,27 @@ defmodule Hpack.Mixfile do
     [app: :hpack,
      version: "0.0.1",
      elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
+     description: description,
+     package: package,
      test_coverage: [tool: Coverex.Task],
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
-  def application do
-    [applications: [:logger, :poison, :benchfella]]
+  defp description do
+    "HPACK implementation for Elixir"
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Kien Nguyen Trung"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/kiennt/hpack",
+      }
+    ]
+  end
+
   defp deps do
     [
       {:poison, "~> 1.5"},
